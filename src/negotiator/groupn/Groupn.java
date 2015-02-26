37272f68b2f558d51b7ee11c43e0187c3f9a4550
Chad Crawford <chad-crawford@utulsa.edu>
2015-02-25 18:59:43 -0600

    Adding some other things

--- src/negotiator/groupn/Groupn.java
+++ src/negotiator/groupn/Groupn.java
@@ -2,19 +2,27 @@ package negotiator.groupn;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
+import negotiator.Bid;
 import negotiator.DeadlineType;
+import negotiator.issue.Issue;
+import negotiator.issue.IssueDiscrete;
 import negotiator.Timeline;
 import negotiator.actions.Accept;
 import negotiator.actions.Action;
 import negotiator.actions.Offer;
 import negotiator.parties.AbstractNegotiationParty;
+import negotiator.Domain;
 import negotiator.utility.UtilitySpace;
 
 /**
  * This is your negotiation party.
  */
 public class Groupn extends AbstractNegotiationParty {
+
+    private final Random rand;
+    private Bid currentBid;
 
 	/**
 	 * Please keep this constructor. This is called by genius.
@@ -29,7 +37,12 @@ public class Groupn extends AbstractNegotiationParty {
 				  Timeline timeline,
 				  long randomSeed) {
 		// Make sure that this constructor calls it's parent.
-		super(utilitySpace, deadlines, timeline, randomSeed);
+        super(utilitySpace, deadlines, timeline, randomSeed);
+        this.rand = new Random(randomSeed);
+        for(Issue i: this.getUtilitySpace().getDomain().getIssues()) {
+            IssueDiscrete di = (IssueDiscrete)i;
+            System.out.format(" %s %d\n", di, di.getNumberOfValues());
+        }
 	}
 
 	/**
@@ -39,17 +52,40 @@ public class Groupn extends AbstractNegotiationParty {
 	 * @param validActions Either a list containing both accept and offer or only offer.
 	 * @return The chosen action.
 	 */
+
+    double Bj = 0;
+    double Uj = 0;
 	@Override
 	public Action chooseAction(List<Class> validActions) {
-
 		// with 50% chance, counter offer
 		// if we are the first party, also offer.
-		if (!validActions.contains(Accept.class) || Math.random() > 0.5) {
-			return new Offer(generateRandomBid());
-		}
-		else {
-			return new Accept();
-		}
+        System.out.println(this.utilitySpace.getDiscountFactor());
+        Uj = Bj*Bj;
+        Bj = 1./2 + Bj*Bj;
+        try {
+            if(currentBid != null && utilitySpace.getUtility(currentBid) >= Uj) {
+                return new Accept();
+            }
+            else {
+                return new Offer(this.utilitySpace.getMaxUtilityBid());
+//                return new Offer(this.generateRandomBid());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+//		if (!validActions.contains(Accept.class) || timeline.getCurrentTime()<timeline.getTotalTime()-0.1) {
+//            try {
+//                return new Offer(this.utilitySpace.getMaxUtilityBid());
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//                return null;
+//            }
+//        }
+//		else {
+//			return new Accept();
+//		}
 	}
 
 
@@ -63,6 +99,7 @@ public class Groupn extends AbstractNegotiationParty {
 	@Override
 	public void receiveMessage(Object sender, Action action) {
 		super.receiveMessage(sender, action);
+        currentBid = Action.getBidFromAction(action);
 		// Here you can listen to other parties' messages		
 	}
 
